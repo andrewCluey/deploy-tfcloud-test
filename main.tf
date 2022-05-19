8740a0688bd9f76ff7b2adaee72f1c01f979224a
Cluey <andrew@asc-solutions.co.uk>
2022-05-19 16:28:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
