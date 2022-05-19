537859d5b02bf5fee60927767ab7f181a19acc74
Cluey <andrew@asc-solutions.co.uk>
2022-05-19 16:19:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 # Configure Terraform to set the required AzureRM provider
 # version and features{} block.
-
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.77.0"
+      source = "hashicorp/azurerm"
+      version = "3.6.0"
     }
   }
 }
@@ -14,20 +13,16 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg_test" {
+resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
 }
 
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.rg_test.name
-  location                 = azurerm_resource_group.rg_test.location
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  tags = {
-    deploymentType = "tfc"
-  }
-}
+  tags = var.tags
