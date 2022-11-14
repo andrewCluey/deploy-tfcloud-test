8594bb8c8bfc425227e974e533f2389fe35d361c
andrew@asc-solutions.co.uk <andrew@andrews-MacBook-Air.local>
2022-11-14 23:04:11 +0000

    changed resource names

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_storage_account" "main" {
   name                     = "sadeploytfcloud${var.environment}001"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags                     = local.assigned_tags
